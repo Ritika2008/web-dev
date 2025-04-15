a03ebef75febbda5bd5679d2b0422b56cd7c9b5b
Ritika2008 <guptaritika6877@gmail.com>
2025-04-15 14:33:45 +0530

    function in js

---
+++ day2.js
@@ -0,0 +1,7 @@
+function tablePrint(){
+    let a=parsInt(document.getElemenetById("Tb1").value);
+    let i;
+    for(i=1;i<=10;i++)
+        output += n+"*"+i+"="+(n*i)+"br">
+        document.getelemenetById ("pass").innerHTML+=`${i*a}`
+}
